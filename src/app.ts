2625d335f9db90d158da2bc880746de6aee94676
Efe Omoregie <marvelcamoarmy@gmail.com>
2022-07-17 20:56:18 +0100

    debugging

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: __dirname.replace('/src', '')+'/.env' });
 import Bot from "./utils/bot";
 import * as types from "@/types/bot";
+import { exit } from "process";
 
 const config: types.TwitterClientConfig = {
   apiKey: process.env.TWITTER_API_KEY!,
@@ -8,6 +9,10 @@ const config: types.TwitterClientConfig = {
   accessToken: process.env.TWITTER_ACCESS_TOKEN!,
   accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
 };
+
+
+ console.log('executed app.ts', config);
+exit(0);
 
 (async function () {
  await new Bot(config).wakeUp();
